29f1a53ee8f46bcfb231f799e117595f158f93ad
Lucca Molon <luccamolon@MacBook-Pro-de-Lucca.local>
2021-03-18 14:58:06 -0300

    menu

--- NanoDev/NanoDev/main.swift
+++ NanoDev/NanoDev/main.swift
@@ -5,23 +5,41 @@
 //  Created by Lucca Molon and Matheus Polonia on 16/03/21.
 //
 
-// funcionalidades -> postar, responder, ver posts, like
+// funcionalidades -> postar, responder, ver posts, like, dislike
 import Foundation
 
-
+let dateFormatter = DateFormatter()
+dateFormatter.dateStyle = .short
+dateFormatter.timeStyle = .short
 
 struct MainPage {
     var allPosts: [Post] = []
     
     mutating func createPost(username: String) -> Void {
-        var post = MainPost(username: username)
-        print("Message: ")
-        let message = readLine()
-        if let m = message {
-            post.text = m
-        }
+        var post = Post(username: username)
+         post.text = readPostText()
         allPosts.append(post)
     }
+    
+    func readPostText() -> String {
+            var arrayPhrases: [String] = []
+
+            print("Enter a post: (enter ˜ to cancel)")
+
+            while let input = readLine() {
+                guard input != "˜" else {
+                    break
+                }
+                arrayPhrases.append(input)
+            }
+            var postText: String = ""
+
+            for p in arrayPhrases {
+                postText += (p + "\n")
+            }
+
+            return postText
+        }
     
     mutating func like(position: Int) -> Void {
         allPosts[position - 1].likes += 1
@@ -51,13 +69,34 @@ struct MainPage {
         }
     }
     
-    func toString(post: Post) {
-        print("\n\(post.username)\n\(post.text)\nlikes: \(post.likes)\n\(post.date)\n")
+    mutating func createReply(position: Int, username: String) {
+        var post = Post(username: username)
+        print("Message: ")
+        let message = readLine()
+        if let m = message {
+            post.text = m
+        }
+        allPosts[position - 1].replies.append(post)
     }
+    
+    func toString(post: Post) {
+        print("\n\(post.username)\n\(post.text)\nlikes: \(post.likes)\n\(dateFormatter.string(from: post.date))\nReplies: \(post.replies.count)")
+    }
+    
+    func titleToString(post: Post) {
+        print("|Author: \(post.username) Title: \(post.title) Replies: \(post.replies.count)|")
+    }
+    
     
     func printAll() {
         for i in allPosts {
             toString(post: i)
+        }
+    }
+    
+    func printAllTitles() {
+        for post in allPosts {
+            titleToString(post: post)
         }
     }
 }
@@ -66,74 +105,58 @@ protocol PostDate {
     var date: Date { get set }
 }
 
-protocol Post: PostDate {
-    var username: String { get set }
-    var text: String { get set }
-    var likes: Int { get set }
-    var replies: [Reply] { get set }
-    var date: Date { get set }
-}
-
-struct MainPost: Post {
+struct Post: PostDate {
     var username: String
+    var title: String = ""
     var text: String = ""
     var likes: Int = 0
-    var replies: [Reply] = []
-    var date: Date = Date()
-}
-
-struct Reply: Post {
-    var username: String
-    var text: String = ""
-    var likes: Int = 0
-    var replies: [Reply] = []
+    var replies: [Post] = []
     var date: Date = Date()
 }
 
 func printMenu() {
-    print("-------------------------")
-    print("| 1 - New Post          |\n| 2 - Like a Post       |\n| 3 - Dislike a Post    |\n| 4 - Reply             |")
-    print("-------------------------")
+    print("------------------------------------------------------------")
+    print("|1| New Post |2| Access Post |3| Like |4| Dislike |˜| Exit")
+    print("------------------------------------------------------------")
 }
 
 func run() {
     var main = MainPage()
+    
     // populate main page
     
     print("Escolha seu username: ")
     let name = readLine()
     while true {
         printMenu()
-        var number = readLine()
+        let number = readLine()
         if let n = number {
-            if Int(n)! <= 0 {
+            if n == "˜" {
                 break
-            }else {
-                switch Int(n) {
-                case 1:
-                    if let username = name {
-                        main.createPost(username: username)
-                    }
-                case 2:
-                    print("Which Post? ")
-                    let p = readLine()
-                    if let post = p {
-                        main.like(position: Int(post)!)
-                    } // tratamento de erro
-                case 3:
-                    print("Which Post? ")
-                    let p = readLine()
-                    if let post = p {
-                        main.dislike(position: Int(post)!)
-                    }
-                case 4:
-                    return
-                    // codar createReply()
-                default:
-                    print("Invalid Input :(")
-                }
             }
-            main.printAll()
+            switch Int(n) {
+            case 1:
+                if let username = name {
+                    main.createPost(username: username)
+                }
+            case 2:
+                return
+            case 3:
+                print("Which Post? ")
+                let p = readLine()
+                if let post = p {
+                    main.like(position: Int(post)!)
+                } // tratamento de erro
+            case 4:
+                print("Which Post? ")
+                let p = readLine()
+                if let post = p {
+                    main.dislike(position: Int(post)!)
+                }
+            default:
+                break
+            }
+            main.printAllTitles()
         }
     }
     
@@ -144,7 +167,7 @@ run()
 //main.createPost(text: "hello1", username: "b", likes: 3) // 1
 //main.createPost(text: "hello2", username: "c", likes: 2) // 2
 //main.createPost(text: "hello3", username: "d", likes: 1) // 3
-
+//
 //main.printAll()
 //print("------")
 //main.like(position: 3)
@@ -159,7 +182,7 @@ run()
 //main.dislike(position: 2)
 //main.printAll()
 //print("------")
-
+//
 //print("Digite seu username: ")
 //var a = readLine()
 //if let string = a {
